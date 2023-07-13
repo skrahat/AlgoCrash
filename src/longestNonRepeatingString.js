f4090cee87b8762c9dd1cc06cd6d519fde201f81
asaduzzaman rahat <asaduzzaman.rahat@mail.mcgill.ca>
2023-07-12 21:58:07 -0400

    lengthOfLongestSubstring algorithm done

---
+++ src/longestNonRepeatingString.js
@@ -0,0 +1,21 @@
+var lengthOfLongestSubstring = function (s) {
+  let maxLength = 0;
+  let left = 0;
+  const charSet = new Set();
+
+  for (let right = 0; right < s.length; right++) {
+    const currentChar = s[right];
+
+    while (charSet.has(currentChar)) {
+      charSet.delete(s[left]);
+      left++;
+    }
+
+    charSet.add(currentChar);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+};
+
+lengthOfLongestSubstring('testssst');
