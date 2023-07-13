8412d6a56d13d989b9b4b5d351327abfdb2cc841
asaduzzaman rahat <asaduzzaman.rahat@mail.mcgill.ca>
2023-07-12 23:17:02 -0400

    LRUcache algorithm done

---
+++ src/LRUcache.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number} capacity
+ */
+
+var LRUCache = function (capacity) {
+  this.myMap = new Map();
+  this.capacity = capacity;
+};
+
+/**
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function (key) {
+  if (!this.cache.has(key)) return -1;
+  const found = this.cache.get(key);
+  this.cache.delete(key);
+  this.cache.set(key, found);
+
+  return this.cache.get(key);
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function (key, value) {
+  //condition to check if key in map
+  if (this.cache.has(key)) {
+    this.cache.delete(key);
+  }
+  this.cache.set(key, value);
+
+  if (this.cache.size > this.capacity) {
+    this.cache.delete(this.cache.keys().next().value); // keys().next().value returns first item's key
+  }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
